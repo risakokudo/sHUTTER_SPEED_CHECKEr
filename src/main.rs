c0a7a8a18925ffdba34b9fe1e86f4332063cfccc
risakokudo <91072518+risakokudo@users.noreply.github.com>
2021-01-24 20:35:19 +0000

    resolve conflicts with master

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 use com_dev_4fx_sor_codecs_sbe::new_order_single_codec::{SBE_BLOCK_LENGTH, SBE_SCHEMA_VERSION};
 use com_dev_4fx_sor_codecs_sbe::{
-    Decoder, Encoder, N
+    Decoder, Encoder, NewOrderSingleDe
