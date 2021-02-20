8fa809d4ea4c778b444c0fd4662c1857f8291610
risakokudo <91072518+risakokudo@users.noreply.github.com>
2021-02-20 18:33:13 +0000

    dx

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 use com_dev_4fx_sor_codecs_sbe::new_order_single_codec::{SBE_BLOCK_LENGTH, SBE_SCHEMA_VERSION};
 use com_dev_4fx_sor_codecs_sbe::{
     Decoder, Encoder, NewOrderSingleDecoder, NewOrderSingleEncoder, OrderSide as SbeOrderSide,
-    Orde
+    OrderType as
