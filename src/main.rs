9d3c9b1d49dee68ad3ef7f8423dafdfddeb0d444
risakokudo <91072518+risakokudo@users.noreply.github.com>
2021-02-20 10:12:40 +0000

    Update functions.js

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 use com_dev_4fx_sor_codecs_sbe::new_order_single_codec::{SBE_BLOCK_LENGTH, SBE_SCHEMA_VERSION};
 use com_dev_4fx_sor_codecs_sbe::{
-    Decoder, Encoder, NewOrderSingleDecoder, NewOrderSingleEncoder, OrderSide a
+    Decoder, Encoder, NewOrderSingleDecoder, NewOrderSingleEncoder, OrderSide as SbeOrderSid
