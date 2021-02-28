d420be1e51b420ce0502f1012535587d429044f1
risakokudo <91072518+risakokudo@users.noreply.github.com>
2021-02-28 11:43:41 +0000

    News updated

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 use com_dev_4fx_sor_codecs_sbe::new_order_single_codec::{SBE_BLOCK_LENGTH, SBE_SCHEMA_VERSION};
 use com_dev_4fx_sor_codecs_sbe::{
     Decoder, Encoder, NewOrderSingleDecoder, NewOrderSingleEncoder, OrderSide as SbeOrderSide,
-    OrderType as SbeOrderType, ReadBuf, Strategy as SbeStrategy, Time
+    OrderType as SbeOrderType, ReadBuf, Strategy as SbeStrategy, TimeInForce as Sbe
