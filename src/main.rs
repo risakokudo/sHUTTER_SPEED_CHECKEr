d8fd3edeef0a484da5ac29fcc26332ff4e8b53cc
risakokudo <91072518+risakokudo@users.noreply.github.com>
2021-03-06 15:32:29 +0000

    adding more files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use com_dev_4fx_sor_codecs_sbe::new_order_single_codec::{SBE_BLOCK_LENGTH, SBE_SCHEMA_VERSION};
 use com_dev_4fx_sor_codecs_sbe::{
     Decoder, Encoder, NewOrderSingleDecoder, NewOrderSingleEncoder, OrderSide as SbeOrderSide,
-    OrderType as SbeOrderType, ReadBuf, Strategy as SbeStrategy, TimeInForce as SbeTimeInForce
+    OrderType as SbeOrderType, ReadBuf, Strategy as SbeStrategy, TimeInForce as SbeTimeInForce,
+    WriteBu
