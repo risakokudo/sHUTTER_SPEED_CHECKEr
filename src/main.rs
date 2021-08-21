1e804df062caed1d3f1999cfb9058993b1a681c0
risakokudo <91072518+risakokudo@users.noreply.github.com>
2021-08-21 14:31:17 +0000

    Updated author

--- src/main.rs
+++ src/main.rs
@@ -85,4 +85,4 @@ impl From<&SbeTimeInForce> for TimeInForce {
             SbeTimeInForce::IOC => Self::IOC,
             SbeTimeInForce::FOK => Self::FOK,
             SbeTimeInForce::GTD => Self::GTD,
-            SbeTimeInForce::NullV
+            SbeTimeInForce::NullVal => Self::NullVal,
