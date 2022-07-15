54b4bc12a5fd83094625d24ed706f3fcf70690bc
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-07-15 07:36:12 +0000

    Reorganization.

--- src/main.rs
+++ src/main.rs
@@ -262,4 +262,4 @@ trait Encode<'a, T: Encoder<'a>> {
 
 impl<'a> Encode<'a, NewOrderSingleEncoder<'a>> for Order {
     fn encode(&self, encoder: &mut NewOrderSingleEncoder<'a>) -> Result<usize, EncodeError> {
-        encoder.cl_ord_id(se
+        encoder.cl_ord_id(self.cl_ord_
