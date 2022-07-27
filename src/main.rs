d38c615fb8b8e7facb34d76bd3287193b5de4a18
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-07-27 11:52:04 +0000

    Start new development cycle

--- src/main.rs
+++ src/main.rs
@@ -265,4 +265,4 @@ impl<'a> Encode<'a, NewOrderSingleEncoder<'a>> for Order {
         encoder.cl_ord_id(self.cl_ord_id);
         encoder.cl_ord_link_id(self.cl_ord_link_id);
         encoder.instrument_id(self.instrument_id);
-        encoder.target_com
+        encoder.target_comp_id(self.target_
