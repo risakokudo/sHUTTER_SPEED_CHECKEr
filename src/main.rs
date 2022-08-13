9163fca180fa22532bf9c871f3c708f006453651
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-08-13 14:23:03 +0000

    Filtering

--- src/main.rs
+++ src/main.rs
@@ -269,4 +269,4 @@ impl<'a> Encode<'a, NewOrderSingleEncoder<'a>> for Order {
         encoder.source_comp_id(self.source_comp_id);
 
         encoder.side(SbeOrderSide::from(&self.side));
-        encoder.order_type(SbeOrderType::from(
+        encoder.order_type(SbeOrderType::from(&self.order
