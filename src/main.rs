8bc9e11edf5c7b28e363107359dbf246e34ef622
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-10-01 13:49:35 +0000

    Update the ChangeLog file

--- src/main.rs
+++ src/main.rs
@@ -288,4 +288,4 @@ trait Decode<'a, T: Decoder<'a>> {
     fn decode(&mut self, decoder: &mut T) -> Result<&Self, DecodeError>;
 }
 
-impl<'a> Decode<'a, NewOrd
+impl<'a> Decode<'a, NewOrderSingleDecoder<
