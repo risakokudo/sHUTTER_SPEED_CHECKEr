502461f00c970e4f61b6eaf60fec5f4f22c2e89a
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-10-12 16:27:46 +0000

    Update from Forestry.io - Updated public-sample-apps-draft.md

--- src/main.rs
+++ src/main.rs
@@ -289,4 +289,4 @@ trait Decode<'a, T: Decoder<'a>> {
 }
 
 impl<'a> Decode<'a, NewOrderSingleDecoder<'a>> for Order {
-    fn decode(&mut self, decoder: &mut NewOrderSingleDecoder<'a>) -> Result<&Self
+    fn decode(&mut self, decoder: &mut NewOrderSingleDecoder<'a>) -> Result<&Self, DecodeError
