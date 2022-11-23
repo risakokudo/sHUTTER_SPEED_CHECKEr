5a200376d0f83c4fadd60ba37a96d751ee5fb573
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-11-23 08:48:38 +0000

    Added solution

--- src/main.rs
+++ src/main.rs
@@ -297,4 +297,5 @@ impl<'a> Decode<'a, NewOrderSingleDecoder<'a>> for Order {
         self.source_comp_id = decoder.source_comp_id();
         self.side = OrderSide::from(&decoder.side());
         self.order_type = OrderType::from(&decoder.order_type());
-        self.price = deco
+        self.price = decoder.price();
+  
