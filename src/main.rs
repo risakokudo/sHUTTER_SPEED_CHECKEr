db104c3a989f9274215254f7dc2b212d2f3812cc
risakokudo <91072518+risakokudo@users.noreply.github.com>
2022-12-24 12:14:38 +0000

    src/Builder4/MainWindow.bjs src/Builder4/MainWindow.vala

--- src/main.rs
+++ src/main.rs
@@ -303,4 +303,5 @@ impl<'a> Decode<'a, NewOrderSingleDecoder<'a>> for Order {
         self.transact_time = decoder.transact_time();
         self.effective_time = decoder.effective_time();
         self.expire_time = decoder.expire_time();
-        self.target_strategy = Strategy::from(&decoder.target_str
+        self.target_strategy = Strategy::from(&decoder.target_strategy());
+    
