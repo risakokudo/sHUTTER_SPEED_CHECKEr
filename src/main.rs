547edc19e7659ddc5d67943579f9abd8fb74d58a
risakokudo <91072518+risakokudo@users.noreply.github.com>
2023-02-02 14:40:47 +0000

    typo?

--- src/main.rs
+++ src/main.rs
@@ -319,4 +319,5 @@ fn main() {
         .with_source_comp_id(13)
         .with_side(OrderSide::BUY)
         .with_order_type(OrderType::MARKET)
-        .with_price(0
+        .with_price(0.6534)
+        
