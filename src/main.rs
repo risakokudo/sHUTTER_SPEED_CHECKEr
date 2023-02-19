41ab11fd733abb0cdbdc065ad911ff85374b3de0
risakokudo <91072518+risakokudo@users.noreply.github.com>
2023-02-19 19:53:02 +0000

    Update chapter1/03_Project_Building.md

--- src/main.rs
+++ src/main.rs
@@ -326,4 +326,4 @@ fn main() {
         .with_effective_time(12345565)
         .with_expire_time(12345565)
         .with_target_strategy(Strategy::VENUE)
-        .with_source_s
+        .with_source_strategy(Strategy::
